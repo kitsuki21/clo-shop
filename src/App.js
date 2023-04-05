ab2d109d218f42e21aab94c74a2325149ace5770
Kolya Voronov <RavenLebron@yandex.by>
2023-04-06 00:19:36 +0300

    add navigation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React from "react";
 import Main from "./pages/Main";
+import Info from "./pages/Info";
+import CartProducts from "./pages/CartProducts";
+import Authorization from "./pages/Authorization";
 
 import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Main />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/info">
+          <Info />
+        </Route>
+        <Route exact path="/cart">
+          <CartProducts />
+        </Route>
+        <Route exact path="/authorization">
+          <Authorization />
+        </Route>
+        <Route path="/">
+          <Main />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,6 +3,7 @@ import st from "./header.module.css";
 import logo from "../../assets/logo.svg";
 import cart from "../../assets/cart.png";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   const countCart = useSelector(
@@ -16,11 +17,20 @@ const Header = () => {
       </div>
       <div className={st.nav}>
         <ul className={st.nav_elem}>
-          <li className={st.elem}>Main</li>
-          <li className={st.elem}>info</li>
-          <li className={st.elem}>Login</li>
           <li className={st.elem}>
-            <img src={cart} alt="cart" />
+            <Link to="/">Main</Link>
+          </li>
+          <li className={st.elem}>
+            <Link to="/info">info</Link>
+          </li>
+          <li className={st.elem}>
+            <Link to="/authorization">Login</Link>
+          </li>
+          <li className={st.elem}>
+            <Link to="/cart">
+              <img src={cart} alt="cart" />
+            </Link>
+
             <span>{countCart.length}</span>
           </li>
         </ul>
